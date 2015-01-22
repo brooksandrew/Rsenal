ea4127beecedf7222bc6b8fd4c82b054351491ff
Andrew Brooks <abrooks@abrooks-mac.local>
2015-01-22 12:12:12 -0500

    adding shinyTable

---
+++ R/shiny_table.R
@@ -0,0 +1,24 @@
+#' @title Shiny app to visualize dataframe as simple interactive datatable
+#' @description Launches a basic Shiny App that renders the given dataframe into an interactive datatable using \code{renderDataTable}
+#' @param df dataframe to be visualized
+#' @return Shiny App
+#' @import shiny
+#' @export
+#' @examples
+#' \dontrun{
+#' shinyTable(mtcars) 
+#' }
+
+shinyTable <- function(df) {
+  shinyApp(
+    shinyUI(basicPage(
+      h2(paste0(deparse(substitute(df)))),
+      dataTableOutput('mytable')
+    )),
+    server = function(input, output) {
+      output$mytable = renderDataTable({
+        df
+      })
+    }
+  )
+}
