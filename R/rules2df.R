97f6495c332b8a8384891126d61d15b500d9f933
Andrew Brooks <andrewbrooksct@gmail.com>
2015-03-06 16:49:36 -0500

    adding list feature to rulesdf

--- R/rules2df.R
+++ R/rules2df.R
@@ -3,6 +3,7 @@
 #' @description Note this function only currently works when the itemsets are of size 1 on the LHS and RHS
 #'
 #' @param rules list of association rules (S4 arules object).  Output of \code{apriori} function.
+#' @param list logical \code{TRUE} or \code{FALSE}.  sets LHS items in a list, rather than one character string
 #' @return association rules in a data.frame
 #' @export
 #' @examples
@@ -11,18 +12,19 @@
 #' ar <- apriori(Adult, parameter = list(supp = 0.5, conf = 0.9, target = "rules"))
 #' df<-rules2df(ar)
 
-rules2df <- function(rules){
-  if(length(setdiff(arules::size(items(rules)), 2)) > 0) print('Warning!!! Picked up some rules with more than item on LHS or RHS. Need 1 item on LHS and RHS.')
-  
+rules2df <- function(rules, list=F){  
   df <- as(rules, 'data.frame')
   df[,1] <- as.character(df[,1])
   df$lhs <- sapply(df[,1], function(x) strsplit(x, split=' => ')[[1]][1])
   df$rhs <- sapply(df[,1], function(x) strsplit(x, split=' => ')[[1]][2])
-  
   df$lhs <- gsub(pattern='\\{', replacement='', x=df$lhs)
   df$lhs <- gsub(pattern='}', replacement='', x=df$lhs)
   df$rhs <- gsub(pattern='\\{', replacement='', x=df$rhs)
   df$rhs <- gsub(pattern='}', replacement='', x=df$rhs)
   
+  if(list==T){
+    df$lhs <- strsplit(as.character(df$lhs), split=',')
+    df$rhs <- strsplit(as.character(df$rhs), split=',')
+  }
   return(df)
 }
