d31e6d09f3858e68e38d580cc0d46a699cba75fe
Andrew Brooks <andrewbrooksct@gmail.com>
2015-06-10 12:30:31 -0400

    adding filter functionality to datatable app

--- R/shiny_table.R
+++ R/shiny_table.R
@@ -9,24 +9,35 @@
 #' shinyTable(mtcars) 
 #' }
 
+
 shinyTable <- function(df) {
   shinyApp(
-    shinyUI(basicPage(
+    shinyUI(fluidPage(
       h2(paste0(deparse(substitute(df)))),
-      sidebarPanel(
+      sidebarLayout(sidebarPanel(
+        textInput('filter', 'Filter rows using R logical operators'),
+        uiOutput('querystatus'),
+        br(),
+        br(),
         checkboxGroupInput('show_vars', 'Columns to show', names(df), selected=names(df))
-        ),
+      ),
       mainPanel(
         dataTableOutput('mytable')
         )
+      )
     )),
     server = function(input, output) {
       output$mytable = renderDataTable({
-        df
+        df1 <- df[,input$show_vars, drop=F]
+        df2 <- try(eval(parse(text=paste0('subset(df1,', input$filter, ')'))))
+        if(class(df2)!='try-error') {dfp <- df2
+                                     output$querystatus <- renderText({'query successfull :)'})
+                                     } else {dfp <- df1
+                                             output$querystatus <- renderText({'query unsuccessful :( <br/> (showing full dataset)'})
+                                     }
+        dfp
       })
     }
   )
 }
 
-
-shinyTable(mtcars)
