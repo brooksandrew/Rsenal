b19cb866220ffe1a29cd44e12e23c94cf9c6ce05
Andrew Brooks <andrewbrooksct@gmail.com>
2015-09-04 18:06:18 -0400

    spark lines working. adding quantiles to partialDep git push

--- draft/partialDependence.R
+++ draft/partialDependence.R
@@ -1,59 +1,92 @@
 library('randomForest')
 df <- mtcars
 df$vs <- factor(df$vs)
-rf <- randomForest(vs~mpg+cyl+drat+qsec, df, ntrees=10)
+rf <- randomForest(vs~mpg+cyl+drat+qsec+disp+gear+carb+hp, df, ntrees=10)
 #reg <- lm(mpg~vs+cyl+drat+qsec, df)
 
 ## Calculate Partial Dependence Plot for classification.  one X variable at a time.
+model <- rf; df<-df; xvar <- 'qsec'; n<-5; target.class='1'; ci=c(.9,.5,.3)
 
-model <- rf; df<-df ;xvar <- 'qsec'; n<-3; target.class='1' # parameter assignment for running without function
-partialDep <- function(model, df, xvar, n=10, target.class='1') {
-  xv <- df[,xvar]
-  xvr <- seq(min(xv), max(xv), length.out=n)
-  dfb <- df[rep(1:nrow(df), each=n), ]
-  dfb[,xvar] <- rep(xvr, nrow(df))
+
+# NEED TO COUNT OBS PER BIN for rug plot
+partialDep <- function(model, df, xvar, n=10, target.class='1', ci=c(.9,.5,.3)) {
+  if(!class(df) %in% 'data.table') df <- data.table(df)
+  xv <- df[,get(xvar)] # x vector of interest
+  xvr <- seq(min(xv), max(xv), length.out=n) # fixed width break points
+  dfb <- df[rep(1:.N, each=n), ] # creating replicate of data for each 
+  dfb[,xvar] <- rep(xvr, nrow(df)) # replacing x variable of interest with range of x var
+  
+  #table(cut(df[,xvar], breaks=xvr, include.lowest = T))
   
   pred <- predict(model, dfb, type='prob')[,target.class]
   
-  retdf <- data.frame(xvar=dfb[,xvar], pred=pred)
-  ret <- aggregate(pred~xvar, retdf,mean)
+  retdf <- data.table(xvar=dfb[,get(xvar)], pred=pred)
+  ci = c(0.5+ci/2, 0.5-ci/2)
+  ret <- retdf[,.(pred=quantile(pred, ci)), by='xvar'][,ci:=rep(ci, n)][,cilev:=abs(ci-0.5)*2]
   return(ret)
 }
 
-pd  <- partialDep(rf, df, xvar='qsec', n=100)
-plot(pd[,1], pd[,2], type='l')
+pd  <- partialDep(rf, df, xvar='qsec', n=5)
 
+plot(pd[,1], pd[,2], type='l')
 library('data.table')
-model <- rf; df<-df ;xvars <-  names(model$forest$ncat) ; n<-3; target.class='1' # parameter assignment for running without function
+model <- rf; df<-df; xvars<-names(model$forest$ncat) ; n<-5; target.class='1'
 i <- 'mpg'
-partialDepAll <- function(model, df, n=10, xvars=NULL, target.class='1') {
+
+partialDepAll <- function(model, df, n=10, xvars=NULL, target.class='1', ci=c(9,5,3)) {
   # currently only works with random forest
   if(is.null(xvars)) xvars <- names(model$forest$ncat)
   ret <- matrix(nrow=length(xvars), ncol=n)
   row.names(ret) <- xvars
   for(i in xvars){
-    print(i)
     pd <- data.table(partialDep(model=model, df=df, xvar=i, n=n, target.class=target.class))
-    agg <- pd[,mean(pred), by=xvar]
-    ret[row.names(ret)==i] <- agg[[2]]
+    ret[row.names(ret)==i] <- pd[[2]]
   }
-  return(data.frame(ret))
+  
+  # producing data for sparklines plot
+  pdall  <- data.table(ret)
+  pdplot <- melt(pdall)
+  pdplot[,x:=as.numeric(variable)]
+  pdplot[,y:=rep(1:length(xvars), n)]
+  pdplot[,varname:=xvars[y]]
+  setkey(pdplot, y,x)
+  pdplot[,valuediff:=value-shift(value), by=y] # getting absolute value of change
+  pdplot[,valuenorm:=(value-min(value))/(max(value)-min(value)), by=y] # normalizing value size
+
+  retL <- list(mediantab=data.frame(ret),
+               pdplot=data.frame(pdplot),
+               citab=data.frame(ci)
+               )
+  
+  return(retL)
 }
 
 # getting data ready for ggplot bubble plot
-(pdall  <- data.table(partialDepAll(rf, df, n=10)))
-pdplot <- melt(pdall)
-pdplot[,x:=as.numeric(variable)]
-pdplot[,y:=rep(1:length(xvars), n)]
-setkey(pdplot, y,x)
-pdplot[,valuediff:=value-shift(value), by=y] # getting absolute value of change
-pdplot[,valuenorm:=(value-min(value))/(max(value)-min(value)), by=y] # normalizing value size
+
+pdL <- partialDepAll(model=rf, n=5, df=df)
+
+partialDepSpark <- function(df, yaxis.fixed=T) {
+  ggplot(df) +
+    geom_line(aes(x=x, y=value, colour=value), lwd=4) +
+    scale_colour_gradientn(colours=colorRampPalette(c('firebrick', 'grey', 'forestgreen'))(n)) +
+    facet_wrap(~varname, scales=ifelse(yaxis.fixed==T, 'fixed', 'free')) +
+    theme_bw()
+}
+
+partialDepSpark(pdL$pdplot, yaxis.fixed=T) + ggtitle('spark lines')
 
 
-library('ggplot2')
-ggplot(pdplot,aes(x,y))+
-  geom_point(aes(size=(valuenorm+.15)*30), shape=21, fill="white")+
-  scale_size_identity()+
+
+
+## bubble plot
+ggplot(pdplot, aes(x=x, y=y, colour=abs(valuenorm))) +
+  geom_point(aes(size=value*40), shape=19) +
+  #scale_colour_gradientn(colours=colorRampPalette(c('gray', 'red'))(10)) +
+  scale_colour_gradient2(low='firebrick', mid='gray', high='forestgreen') + 
+  scale_size_identity() +
   theme(panel.grid.major=element_line(linetype=2,color="black"),
         axis.text.x=element_text(angle=90,hjust=1,vjust=0))
+
+
+
 
